366d0eefec285e7ee8fb75eab81a71c092176b51
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-02 15:48:06 +0300

    Basic form design

--- WorkingWithForms/ContentView.swift
+++ WorkingWithForms/ContentView.swift
@@ -9,8 +9,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var enableLogging = false
+
+    @State private var selectedColor = 0
+    @State private var colors = ["Red", "Green", "Blue"]
+
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            Form {
+                Picker(selection: $selectedColor, label: Text("Select a color")) {
+                    ForEach(0 ..< colors.count) {
+                        Text(self.colors[$0]).tag($0)
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+
+                Toggle(isOn: $enableLogging) {
+                    Text("Enable Logging")
+                }
+
+                Button(action: {
+                // activate theme!
+                }) {
+                    Text("Save changes")
+                }
+            }.navigationBarTitle("Settings")
+        }
     }
 }
 
